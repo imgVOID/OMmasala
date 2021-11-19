4ac021cea5b958b85dc24693cf4d7c5da7c4e830
imgVOID <imgvoid@gmail.com>
2021-11-19 21:18:47 +0200

    responsive design fixes

--- src/components/content/CardModalFooter.jsx
+++ src/components/content/CardModalFooter.jsx
@@ -6,10 +6,10 @@ import {
 const CardModalFooter = props => {
     return <MDBModalFooter className="text-center justify-content-center
                      text-dark bg-light">
-        <h3 className={props.path[2] === "holders" ? "d-none" : "fontCursive"}>
+        <h3 className={`${props.path[2] === "holders" ? "d-none" : "fontCursive"} p-0 m-0`}>
             10 аромапалочек "Om Masala" за {props.card.price}₴
         </h3>
-        <h3 className={props.path[2] !== "holders" ? "d-none" : "fontCursive"}>
+        <h3 className={`${props.path[2] !== "holders" ? "d-none" : "fontCursive"} p-0 m-0`}>
             Подставка для благовоний за {props.card.price}&nbsp;₴
         </h3>
     </MDBModalFooter>
