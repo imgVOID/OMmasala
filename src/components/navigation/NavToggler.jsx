78a5fd305735123007b6734aaa55f06f08112e48
imgVOID <imgvoid@gmail.com>
2021-11-19 21:36:02 +0200

    responsive design fixes

--- src/components/navigation/NavToggler.jsx
+++ src/components/navigation/NavToggler.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 
 const NavToggler = props => {
-    return <MDBNavbarToggler class="custom-toggler navbar-toggler" onClick={props.handleNavCollapse}
+    return <MDBNavbarToggler class="custom-toggler navbar-toggler p-0 m-0" onClick={props.handleNavCollapse}
                       type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
                       aria-expanded={!props.isNavCollapsed} aria-label="Toggle navigation">
         <MDBIcon icon={!props.isNavCollapsed ? 'window-close' : 'bars'} style={{"color": "#d1c8c2"}} fas size="sm"
-                 className={`${props.path[1] === "catalog" ? "" : "pt-2"} py-md-0 py-lg-0`}/>
+                 className={props.path[1] === "catalog" ? "" : ""}/>
     </MDBNavbarToggler>
 }
 
