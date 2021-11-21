8d79390e16171fc0452bba56204f20c72a025941
imgVOID <imgvoid@gmail.com>
2021-11-21 20:12:02 +0200

    button local link

--- src/components/header/HeaderButton.jsx
+++ src/components/header/HeaderButton.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import {MDBBtn} from "mdb-react-ui-kit";
 
 const HeaderButton = props => {
-    const active = props.path[2] === props.name ? "headerBtnDisabled" : ""
+    const disabled = props.path[2] === props.name ? "headerBtnDisabled" : ""
+
     const handleClickHistory = path => {
         props.history.replace(path)
     }
 
     if (props.dNone !== true){
-            return <MDBBtn outline size={props.size} style={props.style} onClick={() => handleClickHistory(props.name)}
-                           color={props.landing ? "warning" : "light"}
-                    className={`${props.bold ? "font-weight-bold" : ""} m-2 text-light text-uppercase ` + active}>
+            return <MDBBtn outline size={props.size} style={props.style}
+                           onClick={() => handleClickHistory(props.name)} color={props.landing ? "warning" : "light"}
+                    className={`${props.bold ? "font-weight-bold" : ""} m-2 text-light text-uppercase ` + disabled}>
                 {props.text}
             </MDBBtn>
     } else {
