4d7becd27474aa8b05e78cfae73ada401bb894f3
imgVOID <imgvoid@gmail.com>
2021-11-21 19:06:43 +0200

    product modal section

--- src/components/content/CardModalBody.jsx
+++ src/components/content/CardModalBody.jsx
@@ -12,7 +12,7 @@ const CardModalBody = props => {
 
     const HeaderTitle = props => {
         return <h5 className="row w-100 m-0 p-0">
-            <span className="col-11">{props.link[0]}</span>
+            <span className="col-11">{props.title}</span>
             <MDBIcon icon="angle-up" size="sm" fas className="col-1 mx-0 px-0 pt-1 pe-lg-0 ps-lg-2 pe-md-0 ps-md-2"/>
         </h5>
     }
@@ -20,14 +20,14 @@ const CardModalBody = props => {
     return (
         <MDBModalBody className="bg-light">
             <MDBAccordion flush>
-                {Object.entries(props.vendors).map((vendor) => {
+                {Object.keys(props.vendors).map((vendorTitle) => {
                     return <MDBAccordionItem
-                        collapseId={vendor[0]} headerTitle={<HeaderTitle link={vendor}/>}
+                        collapseId={vendorTitle} headerTitle={<HeaderTitle title={vendorTitle}/>}
                         className="justify-content-center bg-light text-center">
                         <MDBContainer className="p-0 p-md-3 p-lg-3">
-                            <a className="p-0 b-0" href={props.card.links[vendor[0]]} target="_blank">
+                            <a className="p-0 b-0" href={props.card.links[vendorTitle]} target="_blank">
                                 <MDBBtn block color="light" className="font-weight-bold mt-1" size="lg">
-                                    Заказать на <span>{vendor[0]}</span>
+                                    Заказать на <span>{vendorTitle}</span>
                                 </MDBBtn>
                             </a>
                             <MDBTable className="mb-0 pb-0 bg-light mt-1">
@@ -41,13 +41,13 @@ const CardModalBody = props => {
                                 </MDBTableHead>
                                 <MDBTableBody>
                                     {
-                                        vendor[1]["delivery"].map((delivery, index) => {
+                                        props.vendors[vendorTitle]["delivery"].map((delivery, index) => {
                                             return <tr className=" m-0 p-0">
                                                 <td className="text-center p-0 pt-1 pb-0 border-0">
                                                     {delivery}
                                                 </td>
                                                 <td className="text-center p-0 pt-1 border-0">
-                                                    {vendor[1]["min_amount"][index]}
+                                                    {props.vendors[vendorTitle]["min_amount"][index]}
                                                 </td>
                                             </tr>
                                         })
@@ -55,9 +55,9 @@ const CardModalBody = props => {
                                 </MDBTableBody>
                             </MDBTable>
                             {
-                                props.vendors[vendor[0]].gift
+                                props.vendors[vendorTitle].gift
                                     ? <MDBTypography tag='small' className="p-0 mt-3 mb-0 d-block font-italic">
-                                        {props.vendors[vendor[0]].gift}!
+                                        {props.vendors[vendorTitle].gift}!
                                     </MDBTypography> : ""
                             }
                         </MDBContainer>
